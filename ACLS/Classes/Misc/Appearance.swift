65ba829a7a5b297cc0de5afede65e6e1f507cea7
akonst17 <akonst17@gmail.com>
2022-05-10 19:00:51 +0300

    build 1

---
+++ ACLS/Classes/Misc/Appearance.swift
@@ -0,0 +1,25 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 252, blue: 253)
+    
+    static let mainColor = UIColor(integralRed: 237, green: 107, blue: 102)
+    static let mainColorAlpha = UIColor(integralRed: 237, green: 107, blue: 102, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 103, green: 178, blue: 208)
+    
+    static let progress1Color = UIColor(integralRed: 237, green: 107, blue: 102)
+    static let progress2Color = UIColor(integralRed: 103, green: 178, blue: 208)
+    static let progress3Color = UIColor(integralRed: 43, green: 35, blue: 104)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+}
---
+++ ACLS/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams2.quantresear.ch"
+    static let apiKey = "PaoRb1uDWrOZuHwB"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "cCZowlz9PPDvOCkt"
+
+    static let applicationTag = "EXAM_ACLS_PALS_BLS"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1623173750"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
